5cbe1bb0d52a9bbdb3636f3d16ac94ce7a83b4b1
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-11-12 23:58:02 +0300

    Add project files.

---
+++ UdemyAndroidKotlin.Auth/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UdemyAndroidKotlin.Auth.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
