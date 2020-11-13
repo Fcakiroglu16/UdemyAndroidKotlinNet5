ff8e7b5b5412918a131eb046c64d2efe1de45913
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-11-13 20:27:46 +0300

    UserController.cs doyasının oluşturulması

---
+++ UdemyAndroidKotlin.Auth/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace UdemyAndroidKotlin.Auth.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Authorize(LocalApi.PolicyName)]
+        public IActionResult Test()
+        {
+            return Ok("test ok");
+        }
+    }
+}
