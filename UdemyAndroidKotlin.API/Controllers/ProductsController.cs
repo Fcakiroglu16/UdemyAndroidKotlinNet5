ea0abfcd1673e4996adf0b7847d077f4ef3ca63f
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-11-15 04:31:03 +0300

    ProductsController ve Get Method

---
+++ UdemyAndroidKotlin.API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyAndroidKotlin.API.Models;
+
+namespace UdemyAndroidKotlin.API.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //odata/products
+        public IActionResult Get()
+        {
+            return Ok(_context.Products.AsQueryable());
+        }
+
+        //odata/products(2)
+        public IActionResult Get([FromODataUri] int key)
+        {
+            return Ok(_context.Products.Where(x => x.Id == key));
+        }
+    }
+}
