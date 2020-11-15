93f122199cdfd53b4631d18c12695041773388e7
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-11-15 07:54:41 +0300

    PhotoController ve  PhotoSave methodunun oluşturulması

---
+++ UdemyAndroidKotlin.Photo.API/Controllers/PhotosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UdemyAndroidKotlin.Photo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var randomFileName = Guid.NewGuid().ToString() + Path.GetExtension(photo.FileName);
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", randomFileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream, cancellationToken);
+                }
+                var returnPath = "photos/" + randomFileName;
+
+                return Ok(new { Url = returnPath });
+            }
+            else
+            {
+                return BadRequest("photo is null");
+            }
+        }
+    }
+}
